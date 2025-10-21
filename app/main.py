9bb6cab611f85488e3341dee2dcd51657c44f3da
EduSalazar0 <edutebansalazar95@gmail.com>
2025-10-21 12:49:39 -0500

    Implemento el controlador Vendedor con puntos finales POST y GET

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from .database import engine
 from .models import vendedor_model
+from .controllers import vendedor_controller # Importa el controlador
 
 # Crea las tablas en la base de datos
 vendedor_model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Incluye las rutas del controlador
+app.include_router(vendedor_controller.router)
 
 @app.get("/")
 def read_root():
